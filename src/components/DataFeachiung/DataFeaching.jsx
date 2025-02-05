ffa688c80576c54b4a36a7c1081556c98abb1867
mdkawsar-alam <mdkawsaralam537@gmail.com>
2025-02-05 15:58:37 +0600

    use axios and try case for data fetching

--- src/components/DataFeachiung/DataFeaching.jsx
+++ src/components/DataFeachiung/DataFeaching.jsx
@@ -1,48 +1,101 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
 
 
 
-const DataFeaching = () => {
+// const DataFeaching = () => {
 
 
- const [users,setUsers]=useState([]);
+//  const [users,setUsers]=useState([]);
 
- const getData=()=>{
-     fetch('https://jsonplaceholder.typicode.com/users')
-     .then((res)=>res.json())
-     .then((data)=>setUsers(data.slice(0,5)))
-     .catch((err)=>console.error(" this is data fetching error ", err)
-     )
+//  const getData=()=>{
+//      fetch('https://jsonplaceholder.typicode.com/users')
+//      .then((res)=>res.json())
+//      .then((data)=>setUsers(data.slice(0,5)))
+//      .catch((err)=>console.error(" this is data fetching error ", err)
+//      )
    
- }
+//  }
 
- useEffect(()=>{
-    getData()
- },[])
+//  useEffect(()=>{
+//     getData()
+//  },[])
 
 
  
-  return (
-    <div>
-        <h1> Data Fetching with fetch and Promises </h1>
-        <div className="border p-5">
-{
-    users.map((user)=>( 
-    <div key={user.id}
-    className='p-2 border bg-yellow-400'
-    >
-        <p>name:{user.name}</p>
-        <p> Phone:{user.phone}</p>
-        <p> Email:{user.email}</p>
-        <p> Id:{user.id}</p>
-        <p>userName {user.username}</p>
+//   return (
+//     <div>
+//         <h1> Data Fetching with fetch and Promises </h1>
+//         <div className="border p-5">
+// {
+//     users.map((user)=>( 
+//     <div key={user.id}
+//     className='p-2 border bg-yellow-400'
+//     >
+//         <p>name:{user.name}</p>
+//         <p> Phone:{user.phone}</p>
+//         <p> Email:{user.email}</p>
+//         <p> Id:{user.id}</p>
+//         <p>userName {user.username}</p>
         
 
-    </div>))
-}
+//     </div>))
+// }
              
-        </div>
+//         </div>
 
+//     </div>
+//   )
+// }
+
+// export default DataFeaching
+
+
+
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+const DataFeaching = () => {
+
+    const [data,setData]=useState([])
+
+    const getData  = async()=>{
+
+        try {
+      const res= await   axios.get('https://jsonplaceholder.typicode.com/users') ;  
+      setData(res.data) 
+
+        } catch (error) {
+            console.log(error.message);
+            
+        }
+ 
+    }
+
+    useEffect(()=>{
+        getData()
+    },[])
+
+    console.log(data);
+    
+  return (
+    <div>
+<h1>hello world</h1>
+
+<div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+
+   {
+    data.map((item)=>{
+        return <div
+        className='p-4 bg-slate-500 text-white rounded-xl'
+        key={item.id}>
+             <p>name:{item.name}</p>
+             <p>email:{item.email}</p>
+             <p>userName: {item.username}</p>
+             <p></p>
+        </div>
+    })
+   }
+</div>
     </div>
   )
 }
